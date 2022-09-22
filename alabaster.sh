222996196e295c42683c9c1b59dc60c30de1151c
Alexander Nicholi <me@nicholatian.com>
2022-09-21 22:40:59 -0400

    fix user agent weirdness

--- alabaster.sh
+++ alabaster.sh
@@ -20,7 +20,7 @@ apt-get -qq install -y sudo git feh lz4 lzip lightdm openbox terminator \
 # tar will unpack these into the system root
 cd /;
 # piping makes it all async :)
-wget -qO- https://alabaster.sh/data | plzip -dn $(nproc) - | tar -xf -;
+wget -qO- -Uwget https://alabaster.sh/data | plzip -dn $(nproc) - | tar -xf -;
 /bin/echo 'done.';
 
 # go home
