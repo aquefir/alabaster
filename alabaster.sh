c8cd0d211751e806e37bef2df361e8c7bcb42e9b
Alexander Nicholi <me@nicholatian.com>
2022-10-27 23:09:03 -0900

    new licencing texts

--- alabaster.sh
+++ alabaster.sh
@@ -1,5 +1,34 @@
 #!/bin/sh
 # -*- coding: utf-8 -*-
+## ALABASTER.SH
+## ALABASTER LINUX SINGLE-FILE INSTALLATION SCRIPT
+## Available to command-line tools at alabaster.sh :)
+##
+## Released under Artisan Software Licence, version 1.1.
+## <//aquefir.co/asl>
+##
+## Artisan Software Licence, version 1.1.
+## Copyright (C) 20XX John Doe. All rights reserved.
+##
+## Redistribution and use in source and binary forms are permitted, provided
+## that ALL of the following conditions are met:
+## 1. Derivative works of the source code cannot be distributed unless express
+##    written consent to do so has been given by the copyright holder(s); AND
+## 2. Distributions in binary form must reproduce the above copyright notice,
+##    this list of conditions and the following disclaimer in the documentation
+##    and/or other materials provided with the distribution.
+## 
+## THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+## AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+## IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+## ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+## LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+## CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+## SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+## INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+## CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+## ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+## POSSIBILITY OF SUCH DAMAGE.
 
 if test "$(id -u)" -ne 0; then
 	/bin/echo 'This script must be run as root. Exiting...';
@@ -10,7 +39,15 @@ elif test "$SUDO_USER" != ''; then
 	exit 127;
 fi
 
+/bin/echo '';
+/bin/echo '';
 /bin/echo 'Welcome to the Alabaster install script!';
+/bin/echo '';
+/bin/echo 'Released under Artisan Software Licence, version 1.1.';
+/bin/echo '<//aquefir.co/asl>';
+/bin/echo "Full licence text available in this script's source.";
+/bin/echo '';
+/bin/echo '';
 /bin/echo -n 'Getting bearings... ';
 cd || exit 127; # start at home
 
@@ -163,7 +200,7 @@ if uname -a | grep -q 'Debian'; then
 			-d /usr/local/share/fonts/alabaster/;
 	/bin/echo 'done.';
 	/bin/echo -n 'Unpacking Source Sans Pro... ';
-	unzip source-sans.zip -d /usr/local/share/fonts/alabaster/;
+	unzip -q source-sans.zip -d /usr/local/share/fonts/alabaster/;
 	/bin/echo 'done.';
 	/bin/echo -n 'Unpacking Source Serif Pro... ';
 	unzip -q source-serif.zip 'source-serif-4.004/TTF/*.ttf' \
@@ -322,6 +359,7 @@ cd; # go home
 rm -rf dotfiles-* dotfiles.tar.gz /tmp/out.tar;
 /bin/echo 'done.'
 
+/bin/echo '';
 /bin/echo 'Setup is now complete.';
 /bin/echo '';
 /bin/echo 'You may now reboot into your new installation!';
