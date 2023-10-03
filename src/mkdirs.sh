ebcdeed7c3e8fc1d046c10d8e3465f9c8670d358
Alexander Nicholi <me@nicholatian.com>
2023-10-03 01:13:39 -0400

    add palemoon dir

--- src/mkdirs.sh
+++ src/mkdirs.sh
@@ -15,7 +15,4 @@ mkdir -p \
       /etc/skel/.config/openbox \
       /etc/skel/.config/terminator \
       /etc/skel/.config/tint2 \
-      "$HOME"/.config/gtk-3.0 \
-      "$HOME"/.config/openbox \
-      "$HOME"/.config/terminator \
-      "$HOME"/.config/tint2;
+      '/etc/skel/.moonchild productions/pale moon'
